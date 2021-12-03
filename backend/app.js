8b715c9d21620063724f3341f3345cfa84aca7bf
Gyandendra Prakash <prakash9.2020@gmail.com>
2021-12-03 15:20:14 +0530

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
+
+//cors
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.cookie('cookie2', 'value2', { sameSite: 'none', secure: true });
+
+    next();
+}
+app.use(allowCrossDomain);
 
 //Route imports
 const todos = require("./routes/todoRoute");
